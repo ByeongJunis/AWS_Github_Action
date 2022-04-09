5cfc4f65dda63628e42d5d7b28c052adc74d32ea
ByeongJunis <guqudjun111@gmail.com>
2022-04-10 00:57:02 +0900

    mongodb module 추가

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ module.exports = async function (fastify, opts) {
     options: Object.assign({}, opts)
   })
 
-  const fastify = require('fastify')()
-
 fastify.register(require('fastify-mongodb'), {
   // force to close the mongodb connection when app stopped
   // the default value is false
